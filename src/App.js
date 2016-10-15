d568d0f9a7a7f9500093bad33c1db4bfbe6ffcf0
Lukas Jakubauskas <lukasjakubauskas@gmail.com>
2016-10-15 22:48:27 +0300

    Added intro page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="loader-container circle-pulse-multiple">
+          <div className="loader">
+            <div className="circle-1"></div>
+            <div className="circle-2"></div>
+            <div className="circle-3"></div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main role="main">
+          <h1>Shh... <span className="title">LukasJokubas.lt</span> grows in silence.</h1>
+        </main>
+        <div className="share-buttons">
+          <span className="st_sharethis_large" data-displayText="ShareThis"></span>
+          <span className="st_googleplus_large" data-displayText="Google +"></span>
+          <span className="st_facebook_large" data-displayText="Facebook"></span>
+          <span className="st_twitter_large" data-displayText="Tweet"></span>
+          <span className="st_linkedin_large" data-displayText="LinkedIn"></span>
+          <span className="st_pinterest_large" data-displayText="Pinterest"></span>
+        </div>
       </div>
     );
   }
